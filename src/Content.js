a3e8d47b24fe8ac469be5a7a25cc8b97c12d6ef2
keya254 <brianosiemokeya@gmail.com>
2023-02-21 12:10:36 +0300

    components, events and states

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,63 @@
+import { useState } from "react";
+
 //import { useState } from "react";
 const Content =()=>{
+
+    const [name, setName] = useState("bob")
+    const [count, setCount] = useState(0);
+
     const handleNameChange = () =>{
         const names =["Bob","kevin","carl","jibro"];
         const int = Math.floor(Math.random() * 4);
-        return names[int];
+        setName(names[int]);
+    };
+    const handleclick  = () =>{
+        console.log("button clicked");
+
+    };
+
+    const handleClickusestate = () => {
+        console.log("double click")
+    }
+
+    const handleClick6 = () => {
+        setCount(count+1);
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} Has Clicked the button`);
+    };
+
+    const handleClick3 = (e) => {
+       // console.log(e.target.className);
+        //alert(e.target.innerText)
+        window.open("https://www.mozilla.org/", "mozillaWindow","popup")
+
+    // const handleClick4 = () => {
+    //     console.log(this);
+    // }
+
     };
     return(
     <main>
-        <p>Hello {handleNameChange()}!</p>
+        <p onDoubleClick={handleClickusestate}>Hello {name}!</p>
+
+        <p onDoubleClick={handleClickusestate}>Count {count}!</p>
+
+        <button onClick={handleclick}>click me</button>
+        <button onClick={() => handleClick2("Kevin")}>Kevin</button>
+        <button onClick={() => handleClick2("Jibro")}>jibro</button>
+        <button onClick={() => handleClick2("Daniel")}>Daniel</button>
+
+        <button className="Daniel" onClick={(e) => handleClick3 (e)}>My name is Daniel</button>
+
+        <button className="Daniel" onClick={handleNameChange}>using useState</button>
+
+        <button
+            onClick={handleClick6}>Click Me
+        </button>
+
+        {/* <button onClick={() => this.handleClick4 ()}>my ooh my</button> */}
     </main>
     );
 };
